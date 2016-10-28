cbfe6e894f5b4c617afa2d6c7634becfd2c96497
Lionel Picard <boodmandiouf@gmail.com>
2016-10-28 11:34:09 +0200

    MAJ page/mecene & agenda

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,15 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new PatrimoineBundle\PatrimoineBundle(),
-            new Sonata\CoreBundle\SonataCoreBundle(),
-            new Sonata\BlockBundle\SonataBlockBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-            new Sonata\AdminBundle\SonataAdminBundle(),
-            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
-            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
 
         );
 
--- src/PatrimoineBundle/Controller/MeceneController.php
+++ src/PatrimoineBundle/Controller/MeceneController.php
@@ -121,4 +121,7 @@ class MeceneController extends Controller
             ->getForm()
         ;
     }
+
+
 }
+
