c2e6b9155ed2ea55d508acabd7539f6e3b6d14e9
Lionel Picard <boodmandiouf@gmail.com>
2016-11-04 11:06:03 +0100

    dernières modification avant mecènes new 1

--- src/PatrimoineBundle/Controller/DefaultController.php
+++ src/PatrimoineBundle/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends Controller
         $Request = $this->getRequest();
         if ($Request->getMethod() == "POST") {
             $Subject = $Request->get("Subject");
-            $message = $Request->get("message") . " " . $Request->get("entreprise") . " " . $Request->get("siret") . " " . $Request->get("nom") . " " . $Request->get("prenom") . " " . $Request->get("adresse") . " " . $Request->get("ville") . " " . $Request->get("code_postal") . " " . $Request->get("email") . " " . $Request->get("phone") . " " . $Request->get("don") . " " . $Request->get("comment");
+            $message = " " . $Request->get("message") . " \n\n " . $Request->get("entreprise") . "\n\n " . $Request->get("siret") . " \n\n " . $Request->get("nom") . " \n\n " . $Request->get("prenom") . " \n\n " . $Request->get("adresse") . " \n\n " . $Request->get("ville") . " \n\n" . $Request->get("code_postal") . " \n\n " . $Request->get("email") . " \n\n " . $Request->get("phone") . " \n\n " . $Request->get("don") . " \n\n " . $Request->get("comment") . " \n\n ";
             $message = \Swift_Message::newInstance('Test')
                 ->setSubject($Subject)
                 ->setFrom('fondsperche@gmail.com')
